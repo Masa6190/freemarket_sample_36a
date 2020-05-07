8ce3ae4eb99711fa6aca4d98ccb1459597ed2ff7
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-05-08 00:27:18 +0900

    fix destroy path

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,8 +50,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item = Item.find(params[:id])
+    @item.destroy
   end
 
   private
